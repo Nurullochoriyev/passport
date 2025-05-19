5ad3019a3463ce4ae72405bc2144776b4d2f2904
nurullo <choriyevnurullo0329@gmail.com>
2025-05-19 13:26:29 +0500

    s

--- app/serializers.py
+++ app/serializers.py
@@ -6,5 +6,5 @@ from app.models import Passport
 class PassportSerializer(serializers.ModelSerializer):
     class Meta:
         model=Passport
-        fields=['id','username','phone_number','email','created_ad']
+        fields=['id','first_name','last_name','phone_number','email','created_ad']
         read_only_fields=['created_ad']
